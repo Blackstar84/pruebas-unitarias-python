899daf8539097e451647c8c60e77fb03009790ef
Blackstar84 <carlosalmada84@gmail.com>
2023-05-26 13:30:35 -0400

    se agrega lo referente a la clase de Estructura del proyecto

--- unittest/libunittest.py
+++ unittest/libunittest.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-class Example(unittest.TestCase):
+class TestExample(unittest.TestCase):
 
     def test_suma_numeros(self):
         numero1 = 10
@@ -9,8 +9,15 @@ class Example(unittest.TestCase):
 
         resultado = numero1 + numero2
         # 30
-
+        #Esto es lo mismo que assert como lo usabamos antes
+        # Como estamos heredando de unittest podemos acceder a su método assertEqual
+        # assert resultado == 30
         self.assertEqual(resultado, 30)
 
+    def test_resta_numeros(self):
+        self.assertEqual(30 - 20, 50)
 
-        
+
+
+if __name__ == '__main__':
+    unittest.main()
