71d5197f29cde9887ac52a47c0bde1b04c4fbf84
Blackstar84 <carlosalmada84@gmail.com>
2023-05-30 16:16:46 -0400

    se agrega hasta la calse de ejecutar pruebas unitarias

--- pytest/test_main.py
+++ pytest/test_main.py
@@ -1,4 +1,17 @@
 import pytest
 
-def test_example():
-    assert 10 == 20, 'Lo sentimos, la prueba no ha pasado.'
+""" def test_example():
+    assert 10 == 20, 'Lo sentimos, la prueba no ha pasado.' """
+
+class TestExample():
+
+    def test_suma_dos_numeros(self):
+        assert 10 + 10 == 20, 'Lo sentimos, la suma no es correcta.'
+
+    def test_resta_dos_numeros(self):
+        assert 10 - 10 == 0, 'Lo sentimos, la resta no es correcta'    
+
+
+class TestExample2():
+    def test_multiplicacion_dos_numeros(self):
+        assert 2 * 10 == 20         
