1ae75e41c8fec0f928d7e70c49ad5b30fdfc0476
Blackstar84 <carlosalmada84@gmail.com>
2023-06-01 07:45:11 -0400

    se agrega hasta la clase de saltar pruebas

--- pytest/app/test/test_task.py
+++ pytest/app/test/test_task.py
@@ -7,9 +7,11 @@ from app.task import DueDateError
 
 class TestTask():
     
+    @pytest.mark.news
     def test_task(self):
         assert True
-
+        
+    @pytest.mark.news
     def test_new_task(self):
         due_date = datetime.now() + timedelta(days=1)
         task = Task('Title', 'Description', 'eduardo_gpg', due_date)
@@ -19,8 +21,16 @@ class TestTask():
         assert task.assigned_to == 'eduardo_gpg'
         assert task.due_date == due_date
 
-
+    @pytest.mark.due_date
+    @pytest.mark.errors
     def test_due_date_error(self):
         with pytest.raises(DueDateError):
             due_date = datetime.now() - timedelta(days=1)
             Task('Title', 'Description', 'eduardo_gpg', due_date)
+
+    @pytest.mark.due_date
+    def test_due_date(self):
+        due_date = datetime.now() + timedelta(days=1)
+        task = Task('Title', 'Description', 'eduardo_gpg', due_date)
+
+        assert task.due_date > datetime.now()
