2fa80d50823c4dd5b5274f6c3667ab76145e98aa
Blackstar84 <carlosalmada84@gmail.com>
2023-06-02 11:17:20 -0400

    se agrega la clase de módulo logging

--- logging/main.py
+++ logging/main.py
@@ -1,3 +1,17 @@
+import logging
+
+# DEBUG = 10 = debug
+# INFO = 20 = info
+# WARNING = 30 = warning
+# ERROR = 40 = error
+# CRITICAL = 50 = critical
+
+# logging.basicConfig(level=20)
+# logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.INFO)
+
+
 def suma(numero1: int, numero2: int) -> int:
     """Permite sumar 2 números enteros
 
@@ -9,18 +23,18 @@ def suma(numero1: int, numero2: int) -> int:
         int: _description_
     """
     
-    print('Entramos aquí!!!!')
+    logging.debug('Entramos aquí!!!!')
     
     resultado = numero1 + numero2
     
-    print('Nos encontramos en esta línea')
+    logging.debug('Nos encontramos en esta línea')
     
     return resultado
 
 
 if __name__ == '__main__':
-    print('Antes del llamado de la función')
+    logging.debug('Antes del llamado de la función')
     
     resultado = suma(15, 20)
     
-    print(resultado)
+    logging.info(resultado)
